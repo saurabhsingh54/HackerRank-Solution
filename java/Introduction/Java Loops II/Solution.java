ae0d1519f450bc8f1efc6495a23f2879f29e0006
saurabhsingh54 <47740751+saurabhsingh54@users.noreply.github.com>
2020-04-10 15:32:36 +0530

    Create Solution.java

---
+++ java/Introduction/Java Loops II/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum =0;
+            sum = a + b;
+            System.out.print(sum+" ");
+            for (int j = 1; j<n; j++){
+                sum = sum + (int)(Math.pow(2,j) * b);
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+
+        }
+        in.close();
+    }
+}
